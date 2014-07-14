1670aa3c159e78019303af4c1be50bc6beb33449
Konstantin <Kverde@yandex.ru>
2014-07-14 09:28:22 +0600

    Init

---
+++ src/DbTools.java
@@ -0,0 +1,11 @@
+/**
+ * Created by w on 14.07.2014.
+ */
+public class DbTools
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("Иструменты для базы данных Oracle.");
+
+    }
+}
